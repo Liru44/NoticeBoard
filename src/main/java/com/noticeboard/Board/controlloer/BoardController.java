820f72fbcdbb52abe47073ad9ad63c7c80d01a2d
하현성 <ant167@naver.com>
2025-01-15 20:17:10 +0900

    로그인, 로그아웃 시 게시글 목록 UI 다르게

--- src/main/java/com/noticeboard/Board/controlloer/BoardController.java
+++ src/main/java/com/noticeboard/Board/controlloer/BoardController.java
@@ -1,13 +1,11 @@
 package com.noticeboard.Board.controlloer;
 
-import com.noticeboard.Board.StringUtil;
 import com.noticeboard.Board.component.CustomUserDetails;
 import com.noticeboard.Board.dto.BoardDTO;
 import com.noticeboard.Board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -45,6 +43,8 @@ public class BoardController {
         if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String)) {
             CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
             model.addAttribute("userName", userDetails.getName());
+        } else {
+            model.addAttribute("userName", null);
         }
         return "boardList";
     }
